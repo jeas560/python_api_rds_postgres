d8ec9b5fd1dd182dd55ac6f1f287605540223a93
jeas560 <jeas560@gmail.com>
2023-12-21 17:22:57 -0300

    função `get_data` adicionada e funcionando com variaveis de ambiente, adicionado black formatter para versão de desenvolvimento

---
+++ python_api_rds_postgres/app.py
@@ -0,0 +1,46 @@
+from dotenv import dotenv_values
+from requests import Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+config = dotenv_values(".env")
+key = config["API_KEY"]
+url = config["URL"]
+
+
+def get_data(start, limit, convert, key, url):
+    parameters = {
+        "start": start,
+        "limit": limit,
+        "convert": convert,
+    }
+
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": key,
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+
+        coin_dict = {
+            "name": [coin["name"] for coin in data["data"]],
+            "symbol": [coin["symbol"] for coin in data["data"]],
+            "date_added": [coin["date_added"] for coin in data["data"]],
+            "last_updated": [coin["last_updated"] for coin in data["data"]],
+            "price": [coin["quote"][convert]["price"] for coin in data["data"]],
+            "volume_24h": [
+                coin["quote"][convert]["volume_24h"] for coin in data["data"]
+            ],
+        }
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+
+    print(coin_dict)
+
+
+get_data("1", "10", "USD", key, url)
